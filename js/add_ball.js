ef822e7f1186caf2e9620e92edf455957c8df4de
Antti <halkomahooli@gmail.com>
2012-11-27 14:15:10 +0200

    Siivottu filuja
    Poistettu turhia kommentteja ja alertteja

--- js/add_ball.js
+++ js/add_ball.js
@@ -1,15 +1,3 @@
-/*  * Palautteen anto, joka aukeaa palautepallon luonnista ja sulkeutuu ruksista. * fadeIn ja fadeOut slow  */ /*$(document).ready(function() {
-	$(document).on("click", "#lisaa_pallo",function() {
-		if ($("div#palaute_ikkuna").is(":hidden")) {
-			$("#palaute_ikkuna").fadeIn("slow");
-		} else {
-			$("#sulje_ikkuna").fadeOut("slow");
-		}
-	});
-});*/
-/*
- * palautteenanto toimii fadeIn ja fadeOut :ina, sulkeminen onnistuu t‰ll‰ hetkell‰
- * sek‰ sulje ruksista, ett‰ klikkaamalla uudelleen palautepallon luontia
- * varmaan tuolla ruksisulkemisella menn‰‰n? ei ole tarvetta sulkea palautteenantoa pallonluontipainikkeesta? */
- $(document).ready(function() {	$(document).on("click", "#lisaa_pallo",function() {			if ($("div#palaute_ikkuna").is(":visible")) {			$("#palaute_ikkuna").fadeOut("slow");		} else {			$("#palaute_ikkuna").fadeIn("slow");
-		}	});		$('#sisalto input:radio').addClass('input_hidden');	$('#sisalto label').click(function() {		$(this).addClass('selected').siblings().removeClass('selected');	});});
+
+/* * palautteenanto toimii fadeIn ja fadeOut :ina, sulkeminen onnistuu t‰ll‰ hetkell‰ * sek‰ sulje ruksista, ett‰ klikkaamalla uudelleen palautepallon luonti * varmaan tuolla ruksisulkemisella menn‰‰n? ei ole tarvetta sulkea palautteenantoa pallonluontipainikkeesta?  */
+ $(document).ready(function() {	$(document).on("click", "#lisaa_pallo",function() {			if ($("div#palaute_ikkuna").is(":visible")) {			$("#palaute_ikkuna").fadeOut("slow");		} else {			$("#palaute_ikkuna").fadeIn("slow");		}	});			/*	 * Lis‰t‰‰n palautteen laaduille class, joka m‰‰ritt‰‰ ne olemaan hidden,	 * main.css:ss‰ siirret‰‰n varsinaiset radio inputit reilusti sivulle piiloon	 * ja korvataan ne palautteen laatua kuvaavilla kuvakkeilla, joilla on radio inputtien toiminta	 */	$('#sisalto input:radio').addClass('input_hidden');	$('#sisalto label').click(function() {		$(this).addClass('selected').siblings().removeClass('selected');	});});
