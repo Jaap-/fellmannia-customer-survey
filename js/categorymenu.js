99dddade3eda21915912c4c0702021001f62493b
Sami Ahonen <sami.m.ahonen@lpt.fi>
2012-11-29 09:19:36 +0200

    kategoria valikon skaalautuvuus

--- js/categorymenu.js
+++ js/categorymenu.js
@@ -4,6 +4,7 @@ var width = $(window).width()/3;
 var windowWidth = $(window).width();
 var position = 2;															// indicates selected category
 var catCount = 0;
+var lastCategory;
 
 function layout(){															// adjusts the height of div "content"
 	var height = $(window).height();
@@ -11,37 +12,51 @@ function layout(){															// adjusts the height of div "content"
 }
 
 function changewidth(){														// adjust width of category divs
-	var width = $(window).width()/3;
+	width = $(window).width()/3;
 	$("#container").css("margin-left",+-((position-2)*width)+"px");			// adjusts the category positions 
 	var i;
-	for(i = 1; i <= catCount; i++){											// loops through categorise and adjusts them
-		$("#"+i).css("width",+width+"px");
-		$("#"+i).css("margin-left",+((i-1)*width)+"px");
+	var textwidth = 0;
+	var compare;
+	for(i = 1; i <= lastCategory; i++){										// loops through categories and adjusts them
+		$("#header"+i).css("width",+width+"px");
+		$("#header"+i).css("margin-left",+(-(catCount-i)*width)+"px");
+		compare = $("#header"+i+" > div").innerWidth();
+		if(compare > textwidth){
+			textwidth = compare;
+		}
+	}
+	if(textwidth >= width){
+		for(i = 1; i <= lastCategory; i++){
+			$("#header"+i).css("width",+textwidth+"px");
+			$("#header"+i).css("margin-left",+(-(catCount-i)*textwidth)+"px");
+			windowWidth = $(window).width();
+			$("#container").css("margin-left",+-(((position-2)*textwidth)+(textwidth*3-windowWidth)/2)+"px");
+		}
 	}
 }
 
 function transitionleft(){													// swipe left
-	$("#instruction"+position).hide();
-	var width = $(window).width()/3;
+	$("#instruction"+(catCount-1+position)).hide();
+	width = $(window).width()/3;
 	$("#container").animate({"margin-left":"-="+width+"px"}, "slow");		// category movement by animating margin change
 	position++;																// updates the indicator of selected category
 	if(position>catCount){													// checks if categories are going too far
 		$("#container").animate({"margin-left":"+="+width+"px"}, "slow");	// if categories are going too far move them back
 		position--;															// update position indicator
 	}
-	$("#instruction"+position).show();
+	$("#instruction"+(catCount-1+position)).show();
 }
 
 function transitionright(){													// swipe right
-	$("#instruction"+position).hide();
-	var width = $(window).width()/3;
+	$("#instruction"+(catCount-1+position)).hide();
+	width = $(window).width()/3;
 	$("#container").animate({"margin-left":"+="+width+"px"}, "slow");
 	position--;
 	if(position<1){
 		$("#container").animate({"margin-left":"-="+width+"px"}, "slow");
 		position++;
 	}
-	$("#instruction"+position).show();
+	$("#instruction"+(catCount-1+position)).show();
 }
 
 
@@ -61,12 +76,14 @@ $(document).ready(function(){
 		success: function(data){
 			if(data.status != 0){
 				$(data.categories).each(function(i,item){
-					$('#container').append('<div class="header" id="'+item.id+'"><h1>'+item.name+'</h1></div>');
+					$('#container').append('<div class="header" id="header'+item.id+'"><div class="textcontainer"><h1>'+item.name+'</h1></div></div>');
 					$('#instructions').append('<div class="instructiontext" id="instruction'+item.id+'"><p>'+item.instructiontext+'</p></div>');
-					$("#"+item.id).css("margin-left",+(i*width)+"px");
+					$("#header"+item.id).css("margin-left",+((i-1)*width)+"px");
 					$("#instruction"+item.id).hide();
 					catCount = i+1;
+					lastCategory = item.id;
 				});
+				$("#instruction"+(catCount-1+position)).show();
 				changewidth();				
 			}
 		}
